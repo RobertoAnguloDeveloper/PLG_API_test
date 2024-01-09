9e548a460f9d8c654fb856d9ada4cf0064ae14fa
Roberto Angulo <89711998+RobertoAnguloDeveloper@users.noreply.github.com>
2024-01-08 19:56:49 -0500

    first commit

---
+++ src/main/java/com/plg/apitest1/Apitest1Application.java
@@ -0,0 +1,13 @@
+package com.plg.apitest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apitest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apitest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/plg/apitest1/Apitest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plg.apitest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apitest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
